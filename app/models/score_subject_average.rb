aa4c13adafb19dd0cc536544f6107b8106c24407
2DN <74901304+2DN@users.noreply.github.com>
2021-02-03 22:22:08 +0900

    [add] reviewカラムのサーバーサイド設定

--- app/models/score_subject_average.rb
+++ app/models/score_subject_average.rb
@@ -6,7 +6,8 @@ class ScoreSubjectAverage
                 :book_keep, :basic_info,
                 :science_n_life, :basic_physics, :physics, :basic_chemistry, :chemistry, :basic_earth_science, :earth_science, :basic_biology, :biology, :science,
                 :wld_history_a, :wld_history_b, :dms_history_a, :dms_history_b, :geography_a, :geography_b, :ctp_society, :ethics, :politics_n_economy, :ethics_n_politics_n_economy, :society,
-                :listening, :reading, :english, :language, :user_id, :average_id, :subject_id,
+                :listening, :reading, :english, :language, :review
+                :user_id, :average_id, :subject_id,
                 :avg_title, :avg_jpn_ctp, :avg_jpn_classic, :avg_jpn_chinese, :avg_jpn,
                 :avg_math_1_a, :avg_math_2_b, :avg_math_3_c, :avg_math_1_a, :avg_math_1, :avg_math_2, :avg_math_3, :avg_math_a, :avg_math_b, :avg_math_c, :avg_math,
                 :avg_book_keep, :avg_basic_info,
@@ -136,7 +137,7 @@ class ScoreSubjectAverage
       book_keep: book_keep, basic_info: basic_info,
       science_n_life: science_n_life, basic_physics: basic_physics, physics: physics, basic_chemistry: basic_chemistry, chemistry: chemistry, basic_earth_science: basic_earth_science, earth_science: earth_science, basic_biology: basic_biology, biology: biology, science: science,
       wld_history_a: wld_history_a, wld_history_b: wld_history_b, dms_history_a: dms_history_a, dms_history_b: dms_history_b, geography_a: geography_a, geography_b: geography_b, ctp_society: ctp_society, ethics: ethics, politics_n_economy: politics_n_economy, ethics_n_politics_n_economy: ethics_n_politics_n_economy, society: society, 
-      listening: listening, reading: reading, english: english, language: language, user_id: user_id, average_id: average.id, subject_id: subject.id)
+      listening: listening, reading: reading, english: english, language: language, review: review, user_id: user_id, average_id: average.id, subject_id: subject.id)
   
   end
 
