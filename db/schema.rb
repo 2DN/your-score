e2ffc326a6b1a65c82d5244b7ab70f9a009336a3
2DN <74901304+2DN@users.noreply.github.com>
2021-02-03 14:36:43 +0900

    [g] averagesテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_051422) do
+ActiveRecord::Schema.define(version: 2021_02_03_051837) do
+
+  create_table "averages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "avg_title", null: false
+    t.integer "avg_jpn_ctp"
+    t.integer "avg_jpn_classic"
+    t.integer "avg_jpn_chinese"
+    t.integer "avg_jpn"
+    t.integer "avg_math_1_a"
+    t.integer "avg_math_2_b"
+    t.integer "avg_math_3_c"
+    t.integer "avg_math_1"
+    t.integer "avg_math_2"
+    t.integer "avg_math_3"
+    t.integer "avg_math_a"
+    t.integer "avg_math_b"
+    t.integer "avg_math_c"
+    t.integer "avg_math"
+    t.integer "avg_book_keep"
+    t.integer "avg_basic_info"
+    t.integer "avg_science_n_life"
+    t.integer "avg_basic_physics"
+    t.integer "avg_physics"
+    t.integer "avg_basic_chemistry"
+    t.integer "avg_chemistry"
+    t.integer "avg_basic_earth_science"
+    t.integer "avg_earth_science"
+    t.integer "avg_basic_biology"
+    t.integer "avg_biology"
+    t.integer "avg_science"
+    t.integer "avg_wld_history_a"
+    t.integer "avg_wld_history_b"
+    t.integer "avg_dms_history_a"
+    t.integer "avg_dms_history_b"
+    t.integer "avg_geography_a"
+    t.integer "avg_geography_b"
+    t.integer "avg_ctp_society"
+    t.integer "avg_ethics"
+    t.integer "avg_politics_n_economy"
+    t.integer "avg_ethics_n_politics_n_economy"
+    t.integer "avg_society"
+    t.integer "avg_listening"
+    t.integer "avg_reading"
+    t.integer "avg_english"
+    t.integer "avg_language"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "task_title", null: false
