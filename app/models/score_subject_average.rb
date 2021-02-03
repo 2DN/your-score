728df5073246039b8593c08150696329e8c0e60a
2DN <74901304+2DN@users.noreply.github.com>
2021-02-03 16:54:48 +0900

    [add] アクション、ストロングパラメター設定

--- app/models/score_subject_average.rb
+++ app/models/score_subject_average.rb
@@ -7,14 +7,12 @@ class ScoreSubjectAverage
                 :science_n_life, :basic_physics, :physics, :basic_chemistry, :chemistry, :basic_earth_science, :earth_science, :basic_biology, :biology, :science,
                 :wld_history_a, :wld_history_b, :dms_history_a, :dms_history_b, :geography_a, :geography_b, :ctp_society, :ethics, :politics_n_economy, :ethics_n_politics_n_economy, :society,
                 :listening, :reading, :english, :language, :user_id, :average_id, :subject_id,
-                
                 :avg_title, :avg_jpn_ctp, :avg_jpn_classic, :avg_jpn_chinese, :avg_jpn,
                 :avg_math_1_a, :avg_math_2_b, :avg_math_3_c, :avg_math_1_a, :avg_math_1, :avg_math_2, :avg_math_3, :avg_math_a, :avg_math_b, :avg_math_c, :avg_math,
                 :avg_book_keep, :avg_basic_info,
                 :avg_science_n_life, :avg_basic_physics, :avg_physics, :avg_basic_chemistry, :avg_chemistry, :avg_basic_earth_science, :avg_earth_science, :avg_basic_biology, :avg_biology, :avg_science,
                 :avg_wld_history_a, :avg_wld_history_b, :avg_dms_history_a, :avg_dms_history_b, :avg_geography_a, :avg_geography_b, :avg_ctp_society, :avg_ethics, :avg_politics_n_economy, :avg_ethics_n_politics_n_economy, :avg_society,
                 :avg_listening, :avg_reading, :avg_english, :avg_language,
-                
                 :sub_title, :sub_jpn_ctp, :sub_jpn_classic, :sub_jpn_chinese, :sub_jpn,
                 :sub_math_1_a, :sub_math_2_b, :sub_math_3_c, :sub_math_1_a, :sub_math_1, :sub_math_2, :sub_math_3, :sub_math_a, :sub_math_b, :sub_math_c, :sub_math,
                 :sub_book_keep, :sub_basic_info,
@@ -37,7 +35,7 @@ class ScoreSubjectAverage
       sub_science_n_life: sub_science_n_life, sub_basic_physics: sub_basic_physics, sub_physics: sub_physics, sub_basic_chemistry: sub_basic_chemistry, sub_chemistry: sub_chemistry, sub_basic_earth_science: sub_basic_earth_science, sub_earth_science: sub_earth_science, sub_basic_biology: sub_basic_biology, sub_biology: sub_biology, sub_science: sub_science,
       sub_wld_history_a: sub_wld_history_a, sub_wld_history_b: sub_wld_history_b, sub_dms_history_a: sub_dms_history_a, sub_dms_history_b: sub_dms_history_b, sub_geography_a: sub_geography_a, sub_geography_b: sub_geography_b, sub_ctp_society: sub_ctp_society, sub_ethics: sub_ethics, sub_politics_n_economy: sub_politics_n_economy, sub_ethics_n_politics_n_economy: sub_ethics_n_politics_n_economy, sub_society: sub_society, 
       sub_listening: sub_listening, sub_reading: sub_reading, sub_english: sub_english, sub_language: sub_language)
-    
+
     Score.create(title: title, jpn_ctp: jpn_ctp, jpn_classic: jpn_classic, jpn_chinese: jpn_chinese, jpn: jpn,
       math_1_a: math_1_a, math_2_b: math_2_b, math_3_c: math_3_c, math_1_a: math_1_a, math_1: math_1, math_2: math_2, math_3: math_3, math_a: math_a, math_b: math_b, math_c: math_c, math: math,
       book_keep: book_keep, basic_info: basic_info,
