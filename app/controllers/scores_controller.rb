0e3541c39bbaf8f1531e5fd25ff5fad2832353f8
2DN <74901304+2DN@users.noreply.github.com>
2021-02-03 16:49:07 +0900

    [g] ActiveModel  ScoreSubjectAvegageモデル

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,4 +2,12 @@ class ScoresController < ApplicationController
   before_action :authenticate_user!
   def index
   end
+
+  def new
+    @score_subject_average = ScoreSubjectAverage.new
+  end
+
+  def create
+  end
+
 end
