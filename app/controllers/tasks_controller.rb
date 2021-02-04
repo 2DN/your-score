121e19faf682b0eb323092451120a7065da11436
2DN <74901304+2DN@users.noreply.github.com>
2021-02-04 17:53:04 +0900

    [add] タスク登録機能作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,23 @@
 class TasksController < ApplicationController
   def index
+    @task = Task.new
   end
 
   def create
+    binding.pry
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to action: :index
+    else
+      render :index
+    end
   end
-  
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task_title, :memo, :limit_date).merge(user_id: current_user.id)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   
-  resources :tasks
+  resources :tasks, only: [:index, :create]
   
   resources :subjects
   
