7d66c2806ea71bac1db03805b1f8a0f079f8b922
2DN <74901304+2DN@users.noreply.github.com>
2021-02-06 12:25:36 +0900

    [g] UserTagRelationsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_032217) do
+ActiveRecord::Schema.define(version: 2021_02_06_032407) do
 
   create_table "averages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "avg_title", null: false
@@ -184,6 +184,15 @@ ActiveRecord::Schema.define(version: 2021_02_06_032217) do
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
+  create_table "user_tag_relations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_id"], name: "index_user_tag_relations_on_tag_id"
+    t.index ["user_id"], name: "index_user_tag_relations_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", default: "", null: false
@@ -203,4 +212,6 @@ ActiveRecord::Schema.define(version: 2021_02_06_032217) do
   add_foreign_key "scores", "subjects"
   add_foreign_key "scores", "users"
   add_foreign_key "tasks", "users"
+  add_foreign_key "user_tag_relations", "tags"
+  add_foreign_key "user_tag_relations", "users"
 end
