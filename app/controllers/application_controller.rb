d1cb0a46eba50a5e0db4d570781604053a0ab064
2DN <74901304+2DN@users.noreply.github.com>
2021-02-08 16:59:01 +0900

    [add] username用のストロングパラメター

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-   end
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+  
 end
